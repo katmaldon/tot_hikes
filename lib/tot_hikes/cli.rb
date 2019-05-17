fdb9688f47b2d24c17c21f672f4e2ff89b28f201
Kat Maldon <katharinamaldon@gmail.com>
2019-05-17 16:01:40 +0000

    cli added return for index input

--- lib/tot_hikes/cli.rb
+++ lib/tot_hikes/cli.rb
@@ -21,17 +21,29 @@ class TotHikes::CLI
   end 
   
   def selection 
-    puts "Pick the hike you'd like to know more about or type exit to exit the application."
+    puts "Pick the hike you'd like to know more about (e.g. '1') or type 'exit' to exit the application"
     input = nil
     while input != "exit"
     input = gets.strip.downcase
       if input.to_i > 0
         trail = @trails.scrape_trails[input.to_i-1]
-          puts "#{trail.name} - #{trail.location}"
+          puts "#{trail.name} - #{trail.address}"
       elsif input == "selection" 
         list_trails 
+      elsif input == "1"  
+        @trails.scrape_trails.each_with_index do |trail, i=1|
+          puts "#{trail.location}"
+        end 
+      elsif input == "2"  
+        @trails.scrape_trails.each_with_index do |trail, i=1|
+          puts "#{trail.location}"
+        end 
+      elsif input == "3"  
+        @trails.scrape_trails.each_with_index do |trail, i=1|
+          puts "#{trail.location}"
+        end 
       else 
-        puts "I'm sorry, that's not a valid entry. Type selection to see a list of trails or exit to exit the application."
+        puts "I'm sorry, that's not a valid entry. Type 'selection' to see a list of trails or 'exit' to exit the application."
       end 
     end 
   end 
