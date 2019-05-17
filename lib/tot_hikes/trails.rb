bd8e63731e0cac3ba0236bb1adf3afbcd82d27e1
Kat Maldon <katharinamaldon@gmail.com>
2019-05-17 10:34:29 +0000

    name scrape data

--- lib/tot_hikes/trails.rb
+++ lib/tot_hikes/trails.rb
@@ -1,12 +1,11 @@
 class TotHikes::Hikes 
   
-attr_accessor :name, :address, :distance_from_nyc
+attr_accessor :name, :address
   @@all = [] 
   
-  def initialize(name, address, distance_from_nyc)
+  def initialize(name, address)
     @name = name 
     @address = address
-    @distance_from_nyc = distance_from_nyc
     @@all << self
   end 
   
@@ -22,39 +21,23 @@ attr_accessor :name, :address, :distance_from_nyc
     trails = []
     trails << self.scrape_one 
     trails << self.scrape_two 
-    trails << self.scrape_three
   end 
   
   def self.scrape_one 
-    html = (open("https://kid101.com/10-kid-friendly-scenic-hikes-around-new-york-places-new-york/"))
+    html = (open("https://57hours.com/blog/best-nyc-hiking-trails/"))
     doc = Nokogiri::HTML(html)
-    name = doc.css
+    name = doc.css("h2")[1].text
     location = doc.css
-    miles = doc.css
-    distance_from_nyc = "Located approximately #{miles} miles from NYC"
-    hike_one = self.new(name, location, distance_from_nyc)
+    hike_one = self.new(name, location)
   end 
   
   def self.scrape_two
-    html = (open("https://kid101.com/10-kid-friendly-scenic-hikes-around-new-york-places-new-york/"))
+    html = (open("https://57hours.com/blog/best-nyc-hiking-trails/"))
     doc = Nokogiri::HTML(html)
-    name = doc.css
+    name = doc.css("h2")[2].text
     location = doc.css
-    miles = doc.css
-    distance_from_nyc = "Located approximately #{miles} miles from NYC"
-    hike_one = self.new(name, location, distance_from_nyc)
+    hike_one = self.new(name, location)
   end 
-  
-  def self.scrape_three 
-    html = (open("https://kid101.com/10-kid-friendly-scenic-hikes-around-new-york-places-new-york/"))
-    doc = Nokogiri::HTML(html)
-    name = doc.css
-    location = doc.css
-    miles = doc.css
-    distance_from_nyc = "Located approximately #{miles} miles from NYC"
-    hike_one = self.new(name, location, distance_from_nyc)
-  end 
-  
   
 end 
   
